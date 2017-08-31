e6a057fe8426b988f6c6a867c0125956c232ea91
athanaseus <aramaila@ska.ac.za>
2017-08-31 09:46:57 +0200

    Added device properties, attributes and commands for SKA Alarm class

--- skabase/SKAAlarm/SKAAlarmDevice/SKAAlarmDevice.py
+++ skabase/SKAAlarm/SKAAlarmDevice/SKAAlarmDevice.py
@@ -17,7 +17,8 @@ import PyTango
 from PyTango import DebugIt
 from PyTango.server import run
 from PyTango.server import Device, DeviceMeta
-from PyTango.server import command
+from PyTango.server import attribute, command
+from PyTango.server import device_property
 from PyTango import AttrQuality, DispLevel, DevState
 from PyTango import AttrWriteType, PipeWriteType
 # Additional import
@@ -34,6 +35,51 @@ class SKAAlarmDevice(Device):
     __metaclass__ = DeviceMeta
     # PROTECTED REGION ID(SKAAlarmDevice.class_variable) ENABLED START #
     # PROTECTED REGION END #    //  SKAAlarmDevice.class_variable
+
+    # -----------------
+    # Device Properties
+    # -----------------
+
+    SubAlarmHandlers = device_property(
+        dtype=('str',),
+    )
+
+    AlarmConfigFile = device_property(
+        dtype='str',
+    )
+
+    FormulaConfDevice = device_property(
+        dtype='str',
+    )
+
+    # ----------
+    # Attributes
+    # ----------
+
+    statsNrAlerts = attribute(
+        dtype='int',
+        doc="Number of Alarm alerts",
+    )
+
+    statsNrAlarms = attribute(
+        dtype='int',
+        doc="Number of Alarms active",
+    )
+
+    statsNrNewAlarms = attribute(
+        dtype='int',
+        doc="Number of New active alarms",
+    )
+
+    statsNrUnackAlarms = attribute(
+        dtype='double',
+        doc="Number of unacknowledged alarms",
+    )
+
+    statsNrRtnAlarms = attribute(
+        dtype='double',
+        doc="Number of returned alarms",
+    )
 
     # ---------------
     # General methods
@@ -54,10 +100,110 @@ class SKAAlarmDevice(Device):
         pass
         # PROTECTED REGION END #    //  SKAAlarmDevice.delete_device
 
+    # ------------------
+    # Attributes methods
+    # ------------------
+
+    def read_statsNrAlerts(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.statsNrAlerts_read) ENABLED START #
+        return 0
+        # PROTECTED REGION END #    //  SKAAlarmDevice.statsNrAlerts_read
+
+    def read_statsNrAlarms(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.statsNrAlarms_read) ENABLED START #
+        return 0
+        # PROTECTED REGION END #    //  SKAAlarmDevice.statsNrAlarms_read
+
+    def read_statsNrNewAlarms(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.statsNrNewAlarms_read) ENABLED START #
+        return 0
+        # PROTECTED REGION END #    //  SKAAlarmDevice.statsNrNewAlarms_read
+
+    def read_statsNrUnackAlarms(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.statsNrUnackAlarms_read) ENABLED START #
+        return 0.0
+        # PROTECTED REGION END #    //  SKAAlarmDevice.statsNrUnackAlarms_read
+
+    def read_statsNrRtnAlarms(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.statsNrRtnAlarms_read) ENABLED START #
+        return 0.0
+        # PROTECTED REGION END #    //  SKAAlarmDevice.statsNrRtnAlarms_read
+
+    def initialize_dynamic_attributes(self):
+        self.debug_stream("In initialize_dynamic_attributes()")
+        # PROTECTED REGION ID(SKAAlarmDevice.initialize_dynamic_attributes) ENABLED START #
+        # PROTECTED REGION END #    //  SKAAlarmDevice.initialize_dynamic_attributes
+
+        """   Example to add dynamic attributes
+           Copy inside the folowing protected area to instanciate at startup."""
+        """    For Attribute activeAlerts
+        myactiveAlerts = PyTango.SpectrumAttr('MyactiveAlerts', PyTango.DevString, PyTango.READ, 10000)
+        self.add_attribute(myactiveAlerts,SKAAlarmDevice.read_activeAlerts, None, None)
+        self.attr_activeAlerts_read = [""]
+        """
+        """    For Attribute alarmsActive
+        myalarmsActive = PyTango.SpectrumAttr('MyalarmsActive', PyTango.DevString, PyTango.READ, 10000)
+        self.add_attribute(myalarmsActive,SKAAlarmDevice.read_alarmsActive, None, None)
+        self.attr_alarmsActive_read = [""]
+        """
+
+    def read_activeAlerts(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.activeAlerts_read) ENABLED START #
+        return ['']
+        # PROTECTED REGION END #    //  SKAAlarmDevice.activeAlerts_read
+
+    def read_alarmsActive(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.alarmsActive_read) ENABLED START #
+        return ['']
+        # PROTECTED REGION END #    //  SKAAlarmDevice.alarmsActive_read
 
     # --------
     # Commands
     # --------
+
+    @command(
+    )
+    @DebugIt()
+    def GetAlarmRule(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.GetAlarmRule) ENABLED START #
+        pass
+        # PROTECTED REGION END #    //  SKAAlarmDevice.GetAlarmRule
+
+    @command(
+    )
+    @DebugIt()
+    def GetAlarmData(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.GetAlarmData) ENABLED START #
+        pass
+        # PROTECTED REGION END #    //  SKAAlarmDevice.GetAlarmData
+
+    @command(
+    dtype_in='str', 
+    doc_in="Alarm name", 
+    )
+    @DebugIt()
+    def GetAlarmAdditionalInfo(self, argin):
+        # PROTECTED REGION ID(SKAAlarmDevice.GetAlarmAdditionalInfo) ENABLED START #
+        pass
+        # PROTECTED REGION END #    //  SKAAlarmDevice.GetAlarmAdditionalInfo
+
+    @command(
+    )
+    @DebugIt()
+    def GetAlarmStats(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.GetAlarmStats) ENABLED START #
+        pass
+        # PROTECTED REGION END #    //  SKAAlarmDevice.GetAlarmStats
+
+    @command(
+    dtype_out='str', 
+    doc_out="JSON string", 
+    )
+    @DebugIt()
+    def GetAlertStats(self):
+        # PROTECTED REGION ID(SKAAlarmDevice.GetAlertStats) ENABLED START #
+        return ""
+        # PROTECTED REGION END #    //  SKAAlarmDevice.GetAlertStats
 
 # ----------
 # Run server
--- skabase/SKAAlarm/test/SKAAlarmDevice_test.py
+++ skabase/SKAAlarm/test/SKAAlarmDevice_test.py
@@ -39,7 +39,7 @@ class SKAAlarmDeviceDeviceTestCase(DeviceTestCase):
     # PROTECTED REGION ID(SKAAlarmDevice.test_additionnal_import) ENABLED START #
     # PROTECTED REGION END #    //  SKAAlarmDevice.test_additionnal_import
     device = SKAAlarmDevice
-    properties = {
+    properties = {'SubAlarmHandlers': '', 'AlarmConfigFile': '', 'FormulaConfDevice': '', 
                   }
     empty = None  # Should be []
 
@@ -68,6 +68,66 @@ class SKAAlarmDeviceDeviceTestCase(DeviceTestCase):
         # PROTECTED REGION ID(SKAAlarmDevice.test_Status) ENABLED START #
         self.device.Status()
         # PROTECTED REGION END #    //  SKAAlarmDevice.test_Status
+
+    def test_GetAlarmRule(self):
+        """Test for GetAlarmRule"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_GetAlarmRule) ENABLED START #
+        self.device.GetAlarmRule()
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_GetAlarmRule
+
+    def test_GetAlarmData(self):
+        """Test for GetAlarmData"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_GetAlarmData) ENABLED START #
+        self.device.GetAlarmData()
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_GetAlarmData
+
+    def test_GetAlarmAdditionalInfo(self):
+        """Test for GetAlarmAdditionalInfo"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_GetAlarmAdditionalInfo) ENABLED START #
+        self.device.GetAlarmAdditionalInfo("")
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_GetAlarmAdditionalInfo
+
+    def test_GetAlarmStats(self):
+        """Test for GetAlarmStats"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_GetAlarmStats) ENABLED START #
+        self.device.GetAlarmStats()
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_GetAlarmStats
+
+    def test_GetAlertStats(self):
+        """Test for GetAlertStats"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_GetAlertStats) ENABLED START #
+        self.device.GetAlertStats()
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_GetAlertStats
+
+    def test_statsNrAlerts(self):
+        """Test for statsNrAlerts"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_statsNrAlerts) ENABLED START #
+        self.device.statsNrAlerts
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_statsNrAlerts
+
+    def test_statsNrAlarms(self):
+        """Test for statsNrAlarms"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_statsNrAlarms) ENABLED START #
+        self.device.statsNrAlarms
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_statsNrAlarms
+
+    def test_statsNrNewAlarms(self):
+        """Test for statsNrNewAlarms"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_statsNrNewAlarms) ENABLED START #
+        self.device.statsNrNewAlarms
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_statsNrNewAlarms
+
+    def test_statsNrUnackAlarms(self):
+        """Test for statsNrUnackAlarms"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_statsNrUnackAlarms) ENABLED START #
+        self.device.statsNrUnackAlarms
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_statsNrUnackAlarms
+
+    def test_statsNrRtnAlarms(self):
+        """Test for statsNrRtnAlarms"""
+        # PROTECTED REGION ID(SKAAlarmDevice.test_statsNrRtnAlarms) ENABLED START #
+        self.device.statsNrRtnAlarms
+        # PROTECTED REGION END #    //  SKAAlarmDevice.test_statsNrRtnAlarms
 
 
 # Main execution

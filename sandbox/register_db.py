eb5775a62463903bed006ac0488db96d974d9b16
kmadisa <kmadisa@ska.ac.za>
2017-09-11 16:39:57 +0200

    Addressing PR#22 review comments. Passing the config file name as a command line argument. register_db.py: printing out the devices successfully registered.

--- sandbox/register_db.py
+++ sandbox/register_db.py
@@ -1,49 +1,75 @@
 #!/usr/bin/env python
 import json
+import argparse
 
+import PyTango
 import fandango.tango as tango
 
+
+parser = argparse.ArgumentParser(
+    description="A python scritp used to register TANGO devices and their  properties")
+parser.add_argument("-conf", "--config_file",
+                    help="The config file defining the TANGO devices in the Element.",
+                    required=True)
+registered_devices = []
 
 def register_device(device_name, device_class_name, server_name,
                     server_instance_name):
 
     svr_name = server_name + '/' + server_instance_name
-    print """Attempting to register TANGO device {!r}
-          class: {!r}  server: {!r}.""".format(device_name, device_class_name, svr_name)
-    tango.add_new_device(svr_name, device_class_name, device_name)
+    print """Attempting to register TANGO device {}
+          class: {}  server: {}.""".format(device_name, device_class_name, svr_name)
+    try:
+        tango.add_new_device(svr_name, device_class_name, device_name)
+    except PyTango.DevError:
+        print """Failed to register device {} due to an
+              error with the database""".format(device_name)
+    else:
+        registered_devices.append(device_name)
 
 
 def put_device_property(device_name, device_properties):
 
     for property_name, property_value in device_properties.items():
-        print "Setting device {!r} properties {!r}: {!r}".format(
+        print "Setting device {} properties {}: {}".format(
             device_name, property_name, property_value)
         tango.put_device_property(device_name, {str(property_name): [property_value]})
 
 
-def main():
-    with open("/home/refelt_config.json") as refelt_config_file:
-        refelt_config_data = json.load(refelt_config_file)
+def parse_config_file(opts):
 
-    servers = refelt_config_data['servers']
+    with open(opts.config_file) as elt_config_file:
+        config_data = json.load(elt_config_file)
+
+    servers = config_data['tango_servers']
 
     for server in servers:
         server_name = server['server_name']
         server_instances = server['server_instances']
-
         for server_instance in server_instances:
             server_instance_name = server_instance['instance_name']
-            server_class = server_instance['server_class']
-            class_name = server_class['class_name']
-            devices = server_class['devices']
 
-            for device in devices:
-                device_name = device['device_name']
-                device_properties = device['device_properties']
+            device_classes = server_instance['device_classes']
 
-                register_device(device_name, class_name, server_name,
-                                server_instance_name)
-                put_device_property(device_name, device_properties)
+            for device_class in device_classes:
+                class_name = device_class['class_name']
+                devices = device_class['devices']
+
+                for device_name in devices:
+                    register_device(device_name, class_name, server_name,
+                                    server_instance_name)
+
+
+def print_registered_devices():
+    print "Device(s) successfully registered."
+    print registered_devices
+
+
+def main():
+    args = parser.parse_args()
+    parse_config_file(args)
+    print_registered_devices()
+
 
 if __name__ == "__main__":
     main()
--- sandbox/start_servers.py
+++ sandbox/start_servers.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
-
 import json
+import argparse
 
 import fandango as fn
 
+parser = argparse.ArgumentParser(description="A python script used to start the TANGO"
+                                             "device server processes")
+parser.add_argument("-conf", "--config_file",
+                    help="A configuration file defining the server processes to be run"
+                    "by Starter.", required=True)
+
+
 def main():
+    args = parser.parse_args()
     astor = fn.Astor()
 
-    with open("/home/refelt_config_starter.json") as config_file:
-        facility_data = json.load(config_file)
+    with open(args.config_file) as elt_config_file:
+        facility_data = json.load(elt_config_file)
 
     hosts_data = facility_data["tango_hosts"]
 
@@ -22,8 +30,8 @@ def main():
                 astor.set_server_level(server_instance, host_name,
                                        srv_instance_startup_level)
 
-            print "Starting up the servers on host=", host_name
-            astor.start_servers(servers_list=server_instances, host=host_name)
+        print "Starting up the servers on host=", host_name
+        astor.start_servers(servers_list=server_instances, host=host_name)
 
 if __name__ == "__main__":
     main()

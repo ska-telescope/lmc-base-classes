596f416c90225b72e39693fad5fa4b8b151d2982
Anton Joubert <ajoubert@ska.ac.za>
2017-11-28 22:34:09 +0200

    Skip Ansible ask-become-pass inside Docker container
    Inside the Docker we are running as tango-cs user, with sudo rights
    without a password prompt, so we can skip this step.
    
    JIRA: LMC-426

--- ansible/play-task.sh
+++ ansible/play-task.sh
@@ -12,13 +12,19 @@ else
   else
     hosts_limit="$2"
   fi
+  # Don't request password if running in Docker
+  if [[ -f /.dockerenv ]]; then
+    ask_become_pass=""
+  else
+   ask_become_pass="--ask-become-pass"
+  fi
   if [[ $1 == *.yml ]]; then
       # Run a different yml as per $1, instead of site.yml
-      cmd="ansible-playbook -i hosts $1 --limit $hosts_limit --verbose --ask-become-pass"
+      cmd="ansible-playbook -i hosts $1 --limit $hosts_limit --verbose $ask_become_pass"
   else
     # Replace underscores with dashes for role-tag[-task-id]
     roletask=${1//[_]/-}
-    cmd="ansible-playbook -i hosts site.yml --limit $hosts_limit --tags $roletask --verbose --ask-become-pass"
+    cmd="ansible-playbook -i hosts site.yml --limit $hosts_limit --tags $roletask --verbose  $ask_become_pass"
   fi
 fi
 

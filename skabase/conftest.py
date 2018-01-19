3892281763ac86b8339535a1e5d047cfbab788c2
kmadisa <kmadisa@ska.ac.za>
2018-01-19 15:24:34 +0200

    Removing extra whitespace.

--- skabase/conftest.py
+++ skabase/conftest.py
@@ -41,6 +41,7 @@ def tango_context(request):
     yield tango_context
     tango_context.stop()
 
+
 @pytest.fixture(scope="function")
 def initialize_device(tango_context):
     """Re-initializes the device.

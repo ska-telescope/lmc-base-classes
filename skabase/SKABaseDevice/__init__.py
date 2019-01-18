0374295933833bcb4feb56cacb34efa7385d0a3d
Aditya Dange <adityadange.ska@gmail.com>
2019-01-18 13:11:15 +0530

    Revert "Change made for resolving import problem."
    This reverts commit 681a0e2f6719e6d9db330d61b2e4dc57990b7b63.

--- skabase/SKABaseDevice/__init__.py
+++ skabase/SKABaseDevice/__init__.py
@@ -9,11 +9,10 @@
 
 A generic base device for SKA.
 """
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-# from . import release
-# from .SKABaseDevice import SKABaseDevice, main
+from . import release
+from .SKABaseDevice import SKABaseDevice, main
 
-# __version__ = release.version
-# __version_info__ = release.version_info
-# __author__ = release.author
+__version__ = release.version
+__version_info__ = release.version_info
+__author__ = release.author

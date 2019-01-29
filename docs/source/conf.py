cbeae4d6de066fbaa1d5ca800f3f25a97cd9633c
Aditya Dange <43164915+adityadangeska@users.noreply.github.com>
2019-01-29 16:32:26 +0530

    Add MOCK_MODULES
    Added MOCK_MODULES to mock Tango and PyTango

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,10 @@
 import sys
 import os
 
+# Mock tango modules
+MOCK_MODULES = ['PyTango', 'tango', 'run', 'DeviceMeta', 'command']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

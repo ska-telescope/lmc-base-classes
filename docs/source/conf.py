a49f5d1f94c8e423588c70f6028bbacbe66b8dce
Aditya Dange <43164915+adityadangeska@users.noreply.github.com>
2019-01-30 12:59:17 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
 
 # Mock tango modules
 MOCK_MODULES = ['PyTango', 'tango', 'tango.server', 'run', 'DeviceMeta', 'command',
-                'future', 'future.utils', 'logging']
+                'future', 'future.utils', 'logging', 'logging.handlers']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,

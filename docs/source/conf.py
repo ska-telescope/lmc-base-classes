af9636881842ca6a3e061f1689237b6937b8f51e
Aditya Dange <adityadange.ska@gmail.com>
2019-02-05 11:13:45 +0530

    Update logger mocking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,10 +27,22 @@
 # sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 autodoc_mock_imports = ['PyTango', 'tango', 'tango.server', 'run', 'DeviceMeta', 'command',
-                        'future', 'future.utils', 'logging', 'logging.handlers']
+                        'future', 'future.utils']
 
 import sys
 import os
+
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+# Mock tango modules
+MOCK_MODULES = ['logging', 'logging.handlers']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

a45cf7b6d2565730999781d1be84c0fbc1b9e800
Bruno Morgado <jb.morgado@gmail.com>
2019-03-26 13:25:21 +0000

    workaround for setuptools limitations

--- scripts/validate-metadata.sh
+++ scripts/validate-metadata.sh
@@ -3,38 +3,41 @@
 # PACKAGE METADATA
 # ----------------
 if [[ $(python setup.py --name) == "UNKNOWN" ]] ; then
-    echo "[err] metadata: name missing"
+    echo "[error] metadata: name missing"
     exit 2
 fi
 
 if [[ $(python setup.py --version) == "0.0.0" ]] ; then
-    echo "[err] metadata: version missing"
+    echo "[error] metadata: version missing"
     exit 2
 fi
 
+# TODO: This presently breaks due to setuptools normalizing the package version to something that may not conform to semantic versioning in some cases.
+#       On future versions of setuptools this issue should be solved (a patch was jsut merged) so we can get back to this.
+#       See: https://github.com/pypa/setuptools/issues/308
 if ! python setup.py --version | grep -q -E '^((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$' ; then
-    echo "[err] metadata: version is not according to versioning standards"
-    exit 2
+    echo "[warning] metadata: version is not according to versioning standards"
+    # exit 2
 fi
 
 
 if [[ $(python setup.py --url) == "UNKNOWN" ]] ; then
-    echo "[err] metadata: url missing"
+    echo "[error] metadata: url missing"
     exit 2
 fi
 
 if [[ $(python setup.py --license) == "UNKNOWN" ]] ; then
-    echo "[err] metadata: license missing"
+    echo "[error] metadata: license missing"
     exit 2
 fi
 
 if [[ $(python setup.py --description) == "UNKNOWN" ]] ; then
-    echo "[err] metadata: description missing"
+    echo "[error] metadata: description missing"
     exit 2
 fi
 
 if ! [[ $(python setup.py --classifiers) ]] ; then
-    echo "[err] metadata: classifiers missing"
+    echo "[error] metadata: classifiers missing"
     exit 2
 fi
 
@@ -42,10 +45,8 @@ echo "[info] metadata: all required tags present"
 
 # CONFIRM TAG VERSION
 # -------------------
-# TODO: This presently breaks due to setuptools normalizing the package version to something that may not conform to semantic versioning in some cases.
-#       On future versions of setuptools this issue should be solved (a patch was jsut merged) so we can get back to this.
-#       See: https://github.com/pypa/setuptools/issues/308
+# TODO: To implement when setuptools issue is fixed upstream
 #if [[ $(python setup.py --version) != $CI_COMMIT_TAG ]] ; then
-#    echo "[err] metadata: python package version differs from git tag version"
+#    echo "[error] metadata: python package version differs from git tag version"
 #    exit 2
 #fi

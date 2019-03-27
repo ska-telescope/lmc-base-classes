59b46137319a893fb09a0fcabefa8276c85a5308
SKAJohanVenter <aventer@ska.ac.za>
2019-03-27 16:00:52 +0200

    Limit the setuptools version to N.N.N or N.N.N+description

--- scripts/validate-metadata.sh
+++ scripts/validate-metadata.sh
@@ -15,7 +15,7 @@ fi
 # TODO: This presently breaks due to setuptools normalizing the package version to something that may not conform to semantic versioning in some cases.
 #       On future versions of setuptools this issue should be solved (a patch was jsut merged) so we can get back to this.
 #       See: https://github.com/pypa/setuptools/issues/308
-if ! python setup.py --version | grep -q -E '^((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$' ; then
+if ! python setup.py --version | grep -q -E '^(([0-9]+)\.([0-9]+)\.([0-9]+)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$' ; then
     echo "[warning] metadata: version is not according to versioning standards"
     # exit 2
 fi

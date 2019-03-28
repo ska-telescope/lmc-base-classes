adbf921cf8cbc139a754563d52a17ca4c2db99dc
Bruno Morgado <jb.morgado@gmail.com>
2019-03-28 08:16:04 +0000

    package version set

--- skabase/release.py
+++ skabase/release.py
@@ -8,7 +8,7 @@
 """Release information for lmc-base-classes Python Package"""
 
 name = """lmcbaseclasses"""
-version = "0.0.1"
+version = "0.1.0"
 version_info = version.split(".")
 description = """A set of generic base devices for SKA Telescope."""
 author = "SKA India"

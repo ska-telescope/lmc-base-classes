421494c7164a175fd4773e957b50ca0d1fe38e6a
Aditya Dange <adityadangeska@gmail.com>
2019-03-30 18:25:55 +0530

    Fixed failing unit test cases of SKAObsDevice.

--- skabase/SKAObsDevice/test/SKAObsDevice_test.py
+++ skabase/SKAObsDevice/test/SKAObsDevice_test.py
@@ -83,7 +83,7 @@ class TestSKAObsDevice(object):
         """Test for GetVersionInfo"""
         # PROTECTED REGION ID(SKAObsDevice.test_GetVersionInfo) ENABLED START #
         versionPattern = re.compile(
-            r'SKAObsDevice, lmc-base-classes, [0-9].[0-9].[0-9], '
+            r'SKAObsDevice, lmcbaseclasses, [0-9].[0-9].[0-9], '
             r'A set of generic base devices for SKA Telescope.')
         versionInfo = tango_context.device.GetVersionInfo()
         assert (re.match(versionPattern, versionInfo[0])) != None
@@ -135,7 +135,7 @@ class TestSKAObsDevice(object):
         """Test for buildState"""
         # PROTECTED REGION ID(SKAObsDevice.test_buildState) ENABLED START #
         buildPattern = re.compile(
-            r'lmc-base-classes, [0-9].[0-9].[0-9], '
+            r'lmcbaseclasses, [0-9].[0-9].[0-9], '
             r'A set of generic base devices for SKA Telescope')
         assert (re.match(buildPattern, tango_context.device.buildState)) != None
         # PROTECTED REGION END #    //  SKAObsDevice.test_buildState

2694082c96637fb8ad087be3042ee2973dfa5399
Bruno Morgado <jb.morgado@gmail.com>
2019-08-29 16:51:38 +0100

    moved linting call directly to makefile

--- code-analysis.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "STATIC CODE ANALYSIS"
-echo "===================="
-echo
-
-# FIXME pylint needs to run twice since there is no way go gather the text and xml output at the same time
-pylint -f colorized skabase
-pylint skabase > lint_output.xml

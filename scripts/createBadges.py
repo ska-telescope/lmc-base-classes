4fb04a0fd1a9b2f80eed88a7c4fef9b735309b03
Bruno Morgado <jb.morgado@gmail.com>
2019-09-19 15:36:44 +0100

    test anybagde functionality

---
+++ scripts/createBadges.py
@@ -0,0 +1,22 @@
+import anybadge
+import os
+import sys
+import json
+
+with open("codeMetrics.json", "r") as json_file:
+    data = json.load(json_file)
+
+# Define thresholds
+build_s_t = { 0: "red",
+              1: "green"
+              }
+
+# Extract metric
+last_build_s_m = data["build-status"]["latest"]["status"]
+
+# Create badge
+last_build_s_b = anybadge.Badge('last build', last_build_status, thresholds=thresholds)
+
+# Write badge
+last_build_s_b.write_badge('public/badges/last_build_s.svg')
+

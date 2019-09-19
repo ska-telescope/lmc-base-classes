37c520f59e9062bc8c0a6360080589c48aee3871
Bruno Morgado <jb.morgado@gmail.com>
2019-09-19 15:40:17 +0100

    bugfix

--- scripts/createBadges.py
+++ scripts/createBadges.py
@@ -15,7 +15,7 @@ build_s_t = { 0: "red",
 last_build_s_m = data["build-status"]["latest"]["status"]
 
 # Create badge
-last_build_s_b = anybadge.Badge('last build', last_build_status, thresholds=thresholds)
+last_build_s_b = anybadge.Badge('last build', last_build_s_m, thresholds=build_s_t)
 
 # Write badge
 last_build_s_b.write_badge('public/badges/last_build_s.svg')

8be22bed5db513b1ad5993d5f2b36bc88e9a9bd5
Bruno Morgado <jb.morgado@gmail.com>
2019-09-20 16:19:31 +0100

    added badges

--- scripts/createBadges.py
+++ scripts/createBadges.py
@@ -12,29 +12,54 @@ with open("codeMetrics.json", "r") as json_file:
 
 # Extract metric
 ## 0 for fail, 1 for pass
+label = "last build"
 metric = data["build-status"]["latest"]["status"]
 
 if metric == 0:
     # set text
-    metric_value = "failed"
+    value = "failed"
     # set colour
-    metric_color = "red"
+    color = "red"
 elif metric == 1:
     # set text
-    metric_value = "passed"
+    value = "passed"
     # set colour
-    metric_color = "green"
+    color = "green"
 else:
     print("ERROR: wrong metric value")
     sys.exit()
 
 # Create badge
-badge = anybadge.Badge(label='last build', value=metric_value, default_color=metric_color)
+badge = anybadge.Badge(label=label, value=value, default_color=color)
 
 # Write badge
-badge.write_badge('build/badges/last_build_s.svg')
+badge.write_badge('build/badges/build_last_status.svg')
 
 ###############################################################################
+# LINTING
+
+# Extract metric
+## 0 for fail, 1 for pass
+label = "lint errors"
+metric = data["linting"]["errors"]
+
+if metric == 0:
+    # set text
+    value = metric
+    # set colour
+    color = "green"
+elif metric > 0:
+    # set text
+    value = metric
+    # set colour
+    color = "yellow"
+
+# Create badge
+badge = anybadge.Badge(label=label, value=value, default_color=color)
+
+# Write badge
+badge.write_badge('build/badges/lint_errors.svg')
+
 
 # # Define thresholds
 # build_s_t = { 0: "red",

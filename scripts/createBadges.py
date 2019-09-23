94e888bc794589be81aad16f9ee1bd0e28cdcb2c
Bruno Morgado <jb.morgado@gmail.com>
2019-09-23 16:18:22 +0100

    updated badges, formated code

--- scripts/createBadges.py
+++ scripts/createBadges.py
@@ -10,6 +10,23 @@ with open("codeMetrics.json", "r") as json_file:
 
 ###############################################################################
 # BUILD STATUS
+## GREEN BUILD DATE ===========================================================
+# Extract metric
+label = "green build"
+metric = data["build-status"]["green"]["timestamp"]
+
+timestamp = datetime.fromtimestamp(metric)
+value = timestamp.strftime("%Y/%m/%d %H:%M:%S")
+color = "grey"
+
+# Create badge
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
+
+# Write badge
+badge.write_badge("build/badges/build_green_date.svg", overwrite=True)
+
 ## LAST BUILD STATUS ==========================================================
 # Extract metric
 label = "last build"
@@ -28,7 +45,9 @@ else:
     color = "yellow"
 
 # Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
 
 # Write badge
 badge.write_badge("build/badges/build_last_status.svg", overwrite=True)
@@ -40,81 +59,15 @@ metric = data["build-status"]["last"]["timestamp"]
 
 timestamp = datetime.fromtimestamp(metric)
 value = timestamp.strftime("%Y/%m/%d %H:%M:%S")
-color = "lightgrey"
+color = "grey"
 
 # Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
 
 # Write badge
 badge.write_badge("build/badges/build_last_date.svg", overwrite=True)
-
-## GREEN BUILD DATE ===========================================================
-# Extract metric
-label = "green build"
-metric = data["build-status"]["green"]["timestamp"]
-
-timestamp = datetime.fromtimestamp(metric)
-value = timestamp.strftime("%Y/%m/%d %H:%M:%S")
-color = "lightgrey"
-
-# Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
-
-# Write badge
-badge.write_badge("build/badges/build_green_date.svg", overwrite=True)
-
-###############################################################################
-# LINT
-## ERRORS ======================================================================
-# Extract metric
-label = "lint errors"
-metric = data["lint"]["errors"]
-value = metric
-
-# set colour
-if metric == 0:
-    color = "green"
-elif metric > 0:
-    color = "yellow"
-
-# Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
-
-# Write badge
-badge.write_badge("build/badges/lint_errors.svg", overwrite=True)
-
-## FAILURES ===================================================================
-# Extract metric
-label = "lint failures"
-metric = data["lint"]["failures"]
-value = metric
-
-# set colour
-if metric == 0:
-    color = "green"
-elif metric > 0:
-    color = "red"
-
-# Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
-
-# Write badge
-badge.write_badge("build/badges/lint_failures.svg", overwrite=True)
-
-## TESTS ===================================================================
-# Extract metric
-label = "lint tests"
-metric = data["lint"]["tests"]
-value = metric
-
-# set colour
-color = "lightgrey"
-
-# Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
-
-# Write badge
-badge.write_badge("build/badges/lint_tests.svg", overwrite=True)
 
 ###############################################################################
 # TESTS
@@ -131,7 +84,9 @@ elif metric > 0:
     color = "red"
 
 # Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
 
 # Write badge
 badge.write_badge("build/badges/tests_failed.svg", overwrite=True)
@@ -143,10 +98,12 @@ metric = data["tests"]["total"]
 value = metric
 
 # set colour
-color = "lightgrey"
+color = "grey"
 
 # Create badge
-badge = anybadge.Badge(label=label, value=value, default_color=color, value_prefix=' ', value_suffix=' ')
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
 
 # Write badge
 badge.write_badge("build/badges/tests_total.svg", overwrite=True)
@@ -164,8 +121,72 @@ thresholds = {50: "red", 60: "orange", 80: "yellow", 100: "green"}
 
 # Create badge
 badge = anybadge.Badge(
-    label=label, value=value, thresholds=thresholds, value_format="%.2f", value_prefix=' ', value_suffix="% "
+    label=label,
+    value=value,
+    thresholds=thresholds,
+    value_format="%.2f",
+    value_prefix=" ",
+    value_suffix="% ",
 )
 
 # Write badge
 badge.write_badge("build/badges/coverage.svg", overwrite=True)
+
+###############################################################################
+# LINT
+## ERRORS ======================================================================
+# Extract metric
+label = "lint errors"
+metric = data["lint"]["errors"]
+value = metric
+
+# set colour
+if metric == 0:
+    color = "green"
+elif metric > 0:
+    color = "yellow"
+
+# Create badge
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
+
+# Write badge
+badge.write_badge("build/badges/lint_errors.svg", overwrite=True)
+
+## FAILURES ===================================================================
+# Extract metric
+label = "lint failures"
+metric = data["lint"]["failures"]
+value = metric
+
+# set colour
+if metric == 0:
+    color = "green"
+elif metric > 0:
+    color = "red"
+
+# Create badge
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
+
+# Write badge
+badge.write_badge("build/badges/lint_failures.svg", overwrite=True)
+
+## NO. TESTS ===================================================================
+# Extract metric
+label = "lint tests"
+metric = data["lint"]["tests"]
+value = metric
+
+# set colour
+color = "grey"
+
+# Create badge
+badge = anybadge.Badge(
+    label=label, value=value, default_color=color, value_prefix=" ", value_suffix=" "
+)
+
+# Write badge
+badge.write_badge("build/badges/lint_tests.svg", overwrite=True)

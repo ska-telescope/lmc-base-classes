2a1ae61d40d14c3bdc13a049c0ee7b28738f45c3
Bruno Morgado <jb.morgado@gmail.com>
2019-10-07 11:54:06 +0100

    now builds new badges when pipeline fails

--- scripts/create_badges.py
+++ scripts/create_badges.py
@@ -3,15 +3,6 @@ import os
 import sys
 import json
 from datetime import datetime
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--failed", help="create badges for when the CI pipeline fails")
-args = parser.parse_args()
-
-if args.failed:
-    print("TODO: add badges when failed")
-    sys.exit(0)
 
 with open("ci-metrics.json", "r") as json_file:
     data = json.load(json_file)

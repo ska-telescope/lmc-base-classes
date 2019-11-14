bdd6797b191c032e69b1830b87abb61ad9213070
Aditya Dange <adityadange.ska@gmail.com>
2019-11-14 15:23:20 +0530

    fix_testcases: Remove custom JSON parsing code.

--- skabase/auxiliary/utils.py
+++ skabase/auxiliary/utils.py
@@ -266,45 +266,6 @@ def get_tango_device_type_id(tango_address):
     return tango_address.split('/')[1:3]
 
 
-# JSON `load` and `loads` equivalents, that force all strings to be returned
-# as byte strings, rather than unicode.  This is critical for fields that will
-# be used by TANGO, as it breaks with unicode strings.
-# Solution from:  https://stackoverflow.com/a/33571117
-
-def json_load_byteified(file_handle):
-    """Similar to json.load(), but forces str instead of unicode strings."""
-    return _byteify(
-        json.load(file_handle, object_hook=_byteify),
-        ignore_dicts=True
-    )
-
-
-def json_loads_byteified(json_text):
-    """Similar to json.loads(), but forces str instead of unicode strings."""
-    return _byteify(
-        json.loads(json_text, object_hook=_byteify),
-        ignore_dicts=True
-    )
-
-
-def _byteify(data, ignore_dicts=False):
-    """If this is a unicode string, return its string representation."""
-    if isinstance(data, str):
-        return data.encode('utf-8')
-    # if this is a list of values, return list of byteified values
-    if isinstance(data, list):
-        return [_byteify(item, ignore_dicts=True) for item in data]
-    # if this is a dictionary, return dictionary of byteified keys and values
-    # but only if we haven't already byteified it
-    if isinstance(data, dict) and not ignore_dicts:
-        return {
-            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
-            for key, value in data.items()
-        }
-    # if it's anything else, return it in its original form
-    return data
-
-
 def get_groups_from_json(json_definitions):
     """Return a dict of tango.Group objects matching the JSON definitions.
 
@@ -389,7 +350,7 @@ def get_groups_from_json(json_definitions):
         for json_definition in json_definitions:
             json_definition = json_definition.strip()
             if json_definition:
-                definition = json_loads_byteified(json_definition)
+                definition = json.loads(json_definition)
                 _validate_group(definition)
                 group_name = definition['group_name']
                 groups[group_name] = _build_group(definition)

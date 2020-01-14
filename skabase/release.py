dd71fc48bec2a5dd675524d17f61a8661e9e60a0
Anton Joubert <ajoubert@ska.ac.za>
2020-01-14 16:10:43 +0200

    SAR-55 Refactor SKABaseDevice to use ska_logging library
    Device servers will now call `ska_logging.configure_logging` once.
    If there are multiple devices in the same device server, the first
    device to start will do it.  Some rework was needed to support
    multiple devices - previously the tags field would not have shown
    different names for each device in a single device server.
    
    As the default ska_logging configuration outputs to the console via the
    root logger, the SKABaseDevice no longer has a default target
    of `console::cout`.  The `loggingTargets` attribute should now
    be seen as the "additional" logging targets that can be used
    to supplement those provided by the ska_logging library's
    default configuration.  In practice, it will only make sense to
    add a file or syslog target.

--- skabase/release.py
+++ skabase/release.py
@@ -7,7 +7,7 @@
 """Release information for lmc-base-classes Python Package"""
 
 name = """lmcbaseclasses"""
-version = "0.2.0"
+version = "0.3.1"
 version_info = version.split(".")
 description = """A set of generic base devices for SKA Telescope."""
 author = "SKA India and SARAO"

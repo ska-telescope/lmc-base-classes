e8817164f6dd2e571eee25f41c3f2b52a8bef611
toor <elisabetta.giani@inaf.it>
2020-12-21 09:54:15 +0100

    CT-166: Removed unused module csp_commands.py.

--- src/ska/base/__init__.py
+++ src/ska/base/__init__.py
@@ -14,7 +14,6 @@ __all__ = (
     "SKASubarrayResourceManager",
     "SKATelState",
     "ObsDeviceStateModel",
-    "csp_commands",
     "csp_subelement_state_machine",
     "CspSubElementMaster",
     "CspSubElementObsDevice",

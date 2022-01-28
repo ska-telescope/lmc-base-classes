f54dee4b6a868bd0ab5c2a91cceee00e2b5da6ef
SKAJohanVenter <aventer@ska.ac.za>
2022-01-28 13:04:02 +0200

    SAR-351 Refactored test

--- tests/long_running_tasks/multidevice/multidevice_component_manager.py
+++ tests/long_running_tasks/multidevice/multidevice_component_manager.py
@@ -1,5 +1,8 @@
 """Component Manager for Multi Device."""
+import logging
+from threading import Event
 import time
+from typing import Callable, List
 import tango
 
 from ska_tango_base.base.component_manager import TaskExecutorComponentManager
@@ -9,32 +12,69 @@ from ska_tango_base.executor import TaskStatus
 class MultiDeviceComponentManager(TaskExecutorComponentManager):
     """Component Manager for Multi Device."""
 
-    def non_aborting_lrc(self, argin, task_callback=None):
+    @staticmethod
+    def _non_aborting_lrc(
+        logger: logging.Logger,
+        sleep_time: float,
+        task_callback: Callable = None,
+        task_abort_event: Event = None,
+    ):
+        """Take a long time to run.
+
+        :param logger: logger
+        :type logger: logging.Logger
+        :param sleep_time: Time to sleep between iterations
+        :type sleep_time: float
+        :param task_callback: Update state, defaults to None
+        :type task_callback: Callable, optional
+        :param task_abort_event: Check for abort, defaults to None
+        :type task_abort_event: Event, optional
+        """
+        retries = 45
+        while retries > 0:
+            retries -= 1
+            time.sleep(sleep_time)  # This command takes long
+            logger.info(
+                "In NonAbortingTask repeating %s",
+                retries,
+            )
+        task_callback(status=TaskStatus.COMPLETED, result="non_aborting_lrc OK")
+
+    def non_aborting_lrc(self, argin: float, task_callback: Callable = None):
         """Take a long time to complete.
 
         :param argin: How long to sleep per iteration
-        :type argin: int
+        :type argin: float
         :param task_callback: Update task state, defaults to None
         :type task_callback: Callable, optional
         """
-
-        def noop(logger, argin, task_callback=None, task_abort_event=None):
-            retries = 45
-            while retries > 0:
-                retries -= 1
-                time.sleep(argin)  # This command takes long
-                logger.info(
-                    "In NonAbortingTask repeating %s",
-                    retries,
-                )
-            task_callback(status=TaskStatus.COMPLETED, result="non_aborting_lrc OK")
-
         task_status, response = self.submit_task(
-            noop, args=[self.logger, argin], task_callback=task_callback
+            self._non_aborting_lrc,
+            args=[self.logger, argin],
+            task_callback=task_callback,
         )
         return task_status, response
 
-    def aborting_lrc(self, argin, task_callback=None):
+    @staticmethod
+    def _aborting_lrc(logger, sleep_time, task_callback=None, task_abort_event=None):
+        retries = 45
+        while (not task_abort_event.is_set()) and retries > 0:
+            retries -= 1
+            time.sleep(sleep_time)  # This command takes long
+            logger.info("In NonAbortingTask repeating %s", retries)
+
+        if retries == 0:  # Normal finish
+            task_callback(
+                status=TaskStatus.COMPLETED,
+                result=f"NonAbortingTask completed {sleep_time}",
+            )
+        else:  # Aborted finish
+            task_callback(
+                status=TaskStatus.ABORTED,
+                result=f"NonAbortingTask Aborted {sleep_time}",
+            )
+
+    def aborting_lrc(self, argin: float, task_callback: Callable = None):
         """Abort a task in progress.
 
         :param argin: How long to sleep per iteration
@@ -42,47 +82,68 @@ class MultiDeviceComponentManager(TaskExecutorComponentManager):
         :param task_callback: Update task state, defaults to None
         :type task_callback: Callable, optional
         """
-
-        def noop_abort(logger, argin, task_callback=None, task_abort_event=None):
-            retries = 45
-            while (not task_abort_event.is_set()) and retries > 0:
-                retries -= 1
-                time.sleep(argin)  # This command takes long
-                logger.info("In NonAbortingTask repeating %s", retries)
-
-            if retries == 0:  # Normal finish
-                task_callback(
-                    status=TaskStatus.COMPLETED,
-                    result=f"NonAbortingTask completed {argin}",
-                )
-            else:  # Aborted finish
-                task_callback(
-                    status=TaskStatus.ABORTED, result=f"NonAbortingTask Aborted {argin}"
-                )
-
         task_status, response = self.submit_task(
-            noop_abort, args=[self.logger, argin], task_callback=task_callback
+            self._aborting_lrc, args=[self.logger, argin], task_callback=task_callback
         )
         return task_status, response
 
-    def throw_exc(self, task_callback=None):
+    @staticmethod
+    def _throw_exc(
+        logger: logging.Logger,
+        task_callback: Callable = None,
+        task_abort_event: Event = None,
+    ):
+        """Throw an exception.
+
+        :param logger: logger
+        :type logger: logging.Logger
+        :param task_callback: Update task state, defaults to None
+        :type task_callback: Callable, optional
+        :param task_abort_event: Check for abort, defaults to None
+        :type task_abort_event: Event, optional
+        :raises RuntimeError: Just to indicate an issue
+        """
+        try:
+            logger.info("About to raise an error")
+            raise RuntimeError("Something went wrong")
+        except RuntimeError as err:
+            task_callback(status=TaskStatus.COMPLETED, result=f"{err}")
+
+    def throw_exc(self, task_callback: Callable = None):
         """Illustrate exceptions.
 
         :param task_callback: Update task status
         :type task_callback: Callable
         """
-
-        def noop_exc(logger, task_callback=None, task_abort_event=None):
-            try:
-                logger.info("About to raise an error")
-                raise RuntimeError("Something went wrong")
-            except RuntimeError as err:
-                task_callback(status=TaskStatus.COMPLETED, result=f"{err}")
-
         task_status, response = self.submit_task(
-            noop_exc, args=[self.logger], task_callback=task_callback
+            self._throw_exc, args=[self.logger], task_callback=task_callback
         )
         return task_status, response
+
+    @staticmethod
+    def _show_progress(
+        logger: logging.Logger,
+        sleep_time: float,
+        task_callback: Callable = None,
+        task_abort_event: Event = None,
+    ):
+        """Illustrate progress.
+
+        :param logger: logger
+        :type logger: logging.Logger
+        :param sleep_time: Time to sleep between progress
+        :type sleep_time: float
+        :param task_callback: Update task state, defaults to None
+        :type task_callback: Callable, optional
+        :param task_abort_event: Check for abort, defaults to None
+        :type task_abort_event: Event, optional
+        """
+        task_callback(status=TaskStatus.IN_PROGRESS)
+        for progress in [1, 25, 50, 74, 100]:
+            task_callback(progress=progress)
+            logger.info("Progress %s", progress)
+            time.sleep(sleep_time)
+        task_callback(status=TaskStatus.COMPLETED, result="Progress completed")
 
     def show_progress(self, argin, task_callback=None):
         """Illustrate progress.
@@ -92,21 +153,50 @@ class MultiDeviceComponentManager(TaskExecutorComponentManager):
         :param task_callback: Update task status
         :type task_callback: Callable
         """
-
-        def noop_progress(logger, argin, task_callback=None, task_abort_event=None):
-            task_callback(status=TaskStatus.IN_PROGRESS)
-            for progress in [1, 25, 50, 74, 100]:
-                task_callback(progress=progress)
-                logger.info("Progress %s", progress)
-                time.sleep(argin)
-            task_callback(status=TaskStatus.COMPLETED, result="Progress completed")
-
         task_status, response = self.submit_task(
-            noop_progress, args=[self.logger, argin], task_callback=task_callback
+            self._show_progress, args=[self.logger, argin], task_callback=task_callback
         )
         return task_status, response
 
-    def call_children(self, argin, client_devices, task_callback=None):
+    @staticmethod
+    def _call_children(
+        logger: logging.Logger,
+        sleep_time: float,
+        client_devices: List,
+        task_callback: Callable = None,
+        task_abort_event: Event = None,
+    ):
+        """Call child devices.
+
+        :param logger: logger
+        :type logger: logging.Logger
+        :param sleep_time: How long children should sleep
+        :type sleep_time: float
+        :param client_devices: List of child devices
+        :type client_devices: List
+        :param task_callback: Update task status
+        :type task_callback: Callable, optional
+        :param task_abort_event: Check for abort, defaults to None
+        :type task_abort_event: Event, optional
+        """
+        if client_devices:
+            for device in client_devices:
+                logger.info("Calling child %s", device)
+                proxy = tango.DeviceProxy(device)
+                proxy.CallChildren(sleep_time)
+            task_callback(
+                status=TaskStatus.COMPLETED,
+                result=f"Called children {client_devices}",
+            )
+        else:
+            task_callback(status=TaskStatus.IN_PROGRESS)
+            logger.info("Waiting in leaf device %s", sleep_time)
+            time.sleep(sleep_time)
+            task_callback(status=TaskStatus.COMPLETED, result="Finished leaf node")
+
+    def call_children(
+        self, argin: float, client_devices: List, task_callback: Callable = None
+    ):
         """Call child devices.
 
         :param argin: Time to sleep if this device does not have children.
@@ -116,27 +206,8 @@ class MultiDeviceComponentManager(TaskExecutorComponentManager):
         :param task_callback: Update task status
         :type task_callback: Callable
         """
-
-        def call_lower_level_devices(
-            logger, argin, client_devices, task_callback=None, task_abort_event=None
-        ):
-            if client_devices:
-                for device in client_devices:
-                    logger.info("Calling child %s", device)
-                    proxy = tango.DeviceProxy(device)
-                    proxy.CallChildren(argin)
-                task_callback(
-                    status=TaskStatus.COMPLETED,
-                    result=f"Called children {client_devices}",
-                )
-            else:
-                task_callback(status=TaskStatus.IN_PROGRESS)
-                logger.info("Waiting in leaf device %s", argin)
-                time.sleep(argin)
-                task_callback(status=TaskStatus.COMPLETED, result="Finished leaf node")
-
         task_status, response = self.submit_task(
-            call_lower_level_devices,
+            self._call_children,
             args=[self.logger, argin, client_devices],
             task_callback=task_callback,
         )

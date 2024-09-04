f1567a8302dd7af0150a7b5f26582089a8a3e426
Andrew Bolin <andrew.bolin@csiro.au>
2024-09-04 14:18:05 +1000

    PERENTIE-2592 Reorder imports

--- tests/unit/test_base_device.py
+++ tests/unit/test_base_device.py
@@ -35,7 +35,6 @@ from ska_tango_base.testing.reference import (
     ReferenceBaseComponentManager,
     ReferenceSkaBaseDevice,
 )
-
 from src.ska_tango_base.testing.reference.reference_test_mode_overrides import (
     ReferenceTestModeOverrides,
 )
